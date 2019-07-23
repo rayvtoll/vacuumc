bff303e18a3cd7c2be89e4fe62848bbf67648773
rayvtoll <rayvtoll@gmail.com>
2019-07-23 14:38:25 +0200

    function outside loop

--- app.py
+++ app.py
@@ -3,13 +3,9 @@
 # import variables
 import os, subprocess, time, json, logging
 
-# Vacuum cleaner for closed application and desktop containers 
-while True:
-    dockerPs = 'docker ps --format "{{ json .}}" | jq --slurp .'
-    X = subprocess.check_output(dockerPs, shell=True).decode('utf-8')
-    data = json.loads(str(X))
-    time.sleep(30)
+dockerPs = 'docker ps --format "{{ json .}}" | jq --slurp .'
 
+def vacuumC(data):
     for i in range(0, len(data)):
         if data[i]['Names'].startswith('vcd-'):
             if "seconds" not in data[i]['RunningFor']:
@@ -28,3 +24,10 @@ while True:
                         logging.warning("deleting: " + data[i]['Names'])
                         os.system("docker container rm -f " + data[i]['Names'])
 
+
+# Vacuum cleaner for closed application and desktop containers 
+while True:
+    time.sleep(30)
+    X = subprocess.check_output(dockerPs, shell=True).decode('utf-8')
+    data = json.loads(str(X))
+    vacuumC(data)
